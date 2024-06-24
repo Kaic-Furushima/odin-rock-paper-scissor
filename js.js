f93ec81926a54246a60cc4f845f90cf6d58e85c6
Kaic Furushima <kaic.fbs@gmail.com>
2024-06-24 17:18:37 -0300

    playRound function

--- js.js
+++ js.js
@@ -1,9 +1,12 @@
+let humanPoints = 0;
+let computerPoints = 0;
+let draws = 0;
+
 function getComputerChoice(){
     number = Math.random();
     if(number >= 0 && number < 0.33){
         return "rock";
-    }
-    if(number >= 0.33 && number < 0.66){
+    }else if(number >= 0.33 && number < 0.66){
         return "paper";
     }
     else{
@@ -17,3 +20,44 @@ function getHumanChoice(){
     } while(choice != "rock" && choice != "paper" && choice != "scissor");
     return choice;
 }
+
+function playRound(){
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+
+    console.log(humanChoice);
+    console.log(computerChoice);
+
+    if(computerChoice == humanChoice){
+        draws++;
+        return "Draw";
+    } else {
+        if(computerChoice == "rock"){
+            if(humanChoice == "paper"){
+                humanPoints++;
+                return "Human wins";
+            } else {
+                computerPoints++;
+                return "Computer wins";
+            }
+        } else if(computerChoice == "paper"){
+            if(humanChoice == "scissor"){
+                humanPoints++;
+                return "Human wins";
+            } else {
+                computerPoints++;
+                return "Computer wins";
+            }
+        } else {
+            if(humanChoice == "rock"){
+                humanPoints++;
+                return "Human wins";
+            } else {
+                computerPoints++;
+                return "Computer wins";
+            }
+        }
+    }
+}
+
+console.log(playRound());
